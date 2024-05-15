c6486f3e6a36cdc89a7dd0e1c673a466cf869a75
MisakaMikoto128 <liuyuanlins@outlook.com>
2024-05-15 23:29:43 +0800

    串口发送方法硬件FIFO bug修复

--- HDL/HDL_Uart.c
+++ HDL/HDL_Uart.c
@@ -11,6 +11,8 @@
  */
 #include "HDL_Uart.h"
 #include "CPU_Define.h"
+#include "DSP2833x_Sci.h"
+#include <stddef.h>
 //
 // Defines
 //
@@ -37,23 +39,41 @@ void InitSciaGpio(void);
 void InitScibGpio(void);
 void InitScicGpio(void);
 
-void error(void);
-
 //
 // Globals
 //
-Uint16 sdataA[8];    // Send data for SCI-A
-Uint16 sdataB[8];    // Send data for SCI-B
-Uint16 sdataC[8];    // Send data for SCI-C
-Uint16 rdataA[8];    // Received data for SCI-A
-Uint16 rdataB[8];    // Received data for SCI-B
-Uint16 rdataC[8];    // Received data for SCI-C
-Uint16 rdata_pointA; // Used for checking the received data
-Uint16 rdata_pointB;
-Uint16 rdata_pointC;
+Uint16 sdataA[8]; // Send data for SCI-A
+Uint16 sdataB[8]; // Send data for SCI-B
+Uint16 sdataC[8]; // Send data for SCI-C
+Uint16 rdataA[8]; // Received data for SCI-A
+Uint16 rdataB[8]; // Received data for SCI-B
+Uint16 rdataC[8]; // Received data for SCI-C
 
-static void Scix_Init(volatile struct SCI_REGS *ScixRegs, uint32_t baud, uint32_t wordLen, uint32_t stopBit,
-                      uint32_t parity)
+typedef struct tagCOM_Dev_t
+{
+    COMID_t comId;
+    // 主要是给Modbus使用，因为使用FIFO，发送完成时机不等于释放总线的时机。
+    // 其时机是连续发送一串，然后直到硬件FIFO中的数据完全发送出去了产生回调，回调属于中断函数。
+    // 这里实现依赖于串口的TC中断。
+    // 这意味着如果两个Uart_Write对同一个串口写入的时机过于接近，那么就无法使得一次Uart_Write
+    // 产生一个回调。
+    UartWriteOverCallback_t write_over_callback;
+    void *write_over_callback_args;
+    UartReceiveCharCallback_t receive_char_callback;
+    UartReceiveCharReadyCallback_t receive_char_ready_callback;
+    uint32_t baud;
+    uint32_t wordLen;
+    uint32_t stopBit;
+    uint32_t parity;
+    // 串口是否初始化
+    bool inited;
+    volatile struct SCI_REGS *ScixRegs;
+} COM_Dev_t;
+
+COM_Dev_t _gCOMList[COM_NUM];
+
+static void Scix_Init(volatile struct SCI_REGS *ScixRegs, uint32_t baud,
+                      uint32_t wordLen, uint32_t stopBit, uint32_t parity)
 {
     // setting the word length
     if (wordLen < UART_WORD_LEN_5 || wordLen > UART_WORD_LEN_8)
@@ -96,21 +116,33 @@ static void Scix_Init(volatile struct SCI_REGS *ScixRegs, uint32_t baud, uint32_
     // Disable RX ERR, SLEEP, TXWAKE
     //
     ScixRegs->SCICTL1.all = 0x0003;
-    ScixRegs->SCICTL2.bit.TXINTENA = 0;   // The sending interrupt function was enabled
-    ScixRegs->SCICTL2.bit.RXBKINTENA = 0; // Receiver-buffer break enable
+    ScixRegs->SCICTL2.bit.TXINTENA = 0; // 1h (R/W) = Enable TXRDY interrupt
+    ScixRegs->SCICTL2.bit.RXBKINTENA =
+        0; // 1h (R/W) = Enable RXRDY/BRKDT interrupt
     uint16_t SCI_PRD = 0;
     SCI_PRD = (Uint16)((LSPCLK_FREQ / (baud * 8)) - 1);
     ScixRegs->SCIHBAUD = SCI_PRD >> 8;
     ScixRegs->SCILBAUD = SCI_PRD;
     ScixRegs->SCICCR.bit.LOOPBKENA = 0; // Enable loop back
-    ScixRegs->SCIFFTX.all = 0xC028;
+
+    // 寄存器 SCIFFCT 的 ABD 位和 CDC 位控制自动波特率逻辑，使能 SCIRST
+    // 位使自动波特率逻辑工作 1 SCI FIFO can resume transmit or receive. SCIRST
+    // should be 1 even for Autobaud logic to work. 0 Write 0 to reset the SCI
+    // transmit and receive channels. SCI FIFO register configuration bits will be
+    // left as is.
+    ScixRegs->SCIFFTX.bit.SCIRST = 1; // SCI Reset
+
+    /*
+    SCI FIFO enable Reset type: SYSRSn 0h (R/W) = SCI FIFO enhancements are
+    disabled 1h (R/W) = SCI FIFO enhancements are enabled
+    */
     ScixRegs->SCIFFTX.bit.SCIFFENA = 1;
-    ScibRegs.SCIFFTX.bit.TXFFIENA = 0;  // 发送FIFO中断失能
+    ScibRegs.SCIFFTX.bit.TXFFIENA = 0; // 发送FIFO中断失能
+    ScibRegs.SCIFFTX.bit.TXFFIL = 0;
     ScixRegs->SCIFFRX.bit.RXFFIENA = 1; // 接收FIFO中断使能
     ScixRegs->SCIFFRX.bit.RXFFIL = 1;   // 接收1个字节产生一次发送中断，如果使能了
 
     ScixRegs->SCIFFCT.all = 0x00;
-
     ScixRegs->SCICTL1.all = 0x0023; // Relinquish SCI from Reset
     ScixRegs->SCIFFTX.bit.TXFIFOXRESET = 1;
     ScixRegs->SCIFFRX.bit.RXFIFORESET = 1;
@@ -119,6 +151,19 @@ static void Scix_Init(volatile struct SCI_REGS *ScixRegs, uint32_t baud, uint32_
 void Uart_Init(COMID_t comId, uint32_t baud, uint32_t wordLen, uint32_t stopBit,
                uint32_t parity)
 {
+
+    if (comId >= COM_NUM)
+    {
+        return;
+    }
+
+    COM_Dev_t *pDev = &_gCOMList[comId];
+    pDev->comId = comId;
+    pDev->baud = baud;
+    pDev->wordLen = wordLen;
+    pDev->stopBit = stopBit;
+    pDev->parity = parity;
+
     switch (comId)
     {
     case COM1:
@@ -144,6 +189,7 @@ void Uart_Init(COMID_t comId, uint32_t baud, uint32_t wordLen, uint32_t stopBit,
 
         // Enable CPU int8 and int9 which are connected to SCIs
         IER |= M_INT9;
+
         break;
     case COM2:
         // Init COM2
@@ -188,6 +234,12 @@ void Uart_Init(COMID_t comId, uint32_t baud, uint32_t wordLen, uint32_t stopBit,
     default:
         break;
     }
+
+    pDev->inited = true;
+    pDev->write_over_callback = NULL;
+    pDev->write_over_callback_args = NULL;
+    pDev->receive_char_callback = NULL;
+    pDev->receive_char_ready_callback = NULL;
 }
 
 uint32_t Uart_Write(COMID_t comId, const byte_t *writeBuf, uint32_t uLen)
@@ -196,15 +248,22 @@ uint32_t Uart_Write(COMID_t comId, const byte_t *writeBuf, uint32_t uLen)
     switch (comId)
     {
     case COM1:
+    {
         for (cnt = 0; cnt < uLen; cnt++)
         {
             SciaRegs.SCITXBUF = writeBuf[cnt];
-            // 在此做判断，如果发送FIFO缓冲中数据  >= 16字节，要等待下直到FIFO小于16才能再次向FIFO中存数据
+            // 在此做判断，如果发送FIFO缓冲中数据  >=
+            // 16字节，要等待下直到FIFO小于16才能再次向FIFO中存数据
             while (SciaRegs.SCIFFTX.bit.TXFFST >= 16)
             {
             }
         }
-        break;
+        //  TX FIFO Interrupt Enable
+        SciaRegs.SCIFFTX.bit.TXFFIENA = 1;
+        // TODO:这里如果cnt = 1,在调试模式好像会占用发送一个字节的时间，暂时无法解决
+    }
+
+    break;
     case COM2:
         for (cnt = 0; cnt < uLen; cnt++)
         {
@@ -213,6 +272,8 @@ uint32_t Uart_Write(COMID_t comId, const byte_t *writeBuf, uint32_t uLen)
             {
             }
         }
+        //  TX FIFO Interrupt Enable
+        ScibRegs.SCIFFTX.bit.TXFFIENA = 1;
         break;
     case COM3:
         for (cnt = 0; cnt < uLen; cnt++)
@@ -222,6 +283,8 @@ uint32_t Uart_Write(COMID_t comId, const byte_t *writeBuf, uint32_t uLen)
             {
             }
         }
+        //  TX FIFO Interrupt Enable
+        ScicRegs.SCIFFTX.bit.TXFFIENA = 1;
         break;
     default:
         break;
@@ -232,11 +295,81 @@ uint32_t Uart_Write(COMID_t comId, const byte_t *writeBuf, uint32_t uLen)
 uint32_t Uart_Read(COMID_t comId, byte_t *pBuf, uint32_t uiLen);
 uint32_t Uart_AvailableBytes(COMID_t comId);
 uint32_t Uart_EmptyReadBuffer(COMID_t comId);
+
+/**
+ * @brief
+ * 注册Uart_Write写入完成回调函数。需要注意的是两次Uart_Write时间间隔太短可能就不能
+ * 对应每个Uart_Write调用产生中断。
+ *
+ * @param comId 串口号。
+ * @param callback 回调函数指针。
+ * @param args 回调函数参数。
+ * @return byte_t 成功1，失败0.
+ */
 byte_t Uart_SetWriteOverCallback(COMID_t comId,
-                                 UartWriteOverCallback_t callback, void *args);
+                                 UartWriteOverCallback_t callback, void *args)
+{
+    byte_t ret = 0;
+
+    if (comId < COM_NUM)
+    {
+        _gCOMList[comId].write_over_callback = callback;
+        _gCOMList[comId].write_over_callback_args = args;
+        ret = 1;
+    }
+
+    return ret;
+}
+
+/**
+ * @brief
+ *
+ * @param comId 串口号。
+ * @param callback 回调函数指针。
+ * @return byte_t 成功1，失败0.
+ */
 byte_t Uart_RegisterReceiveCharCallback(COMID_t comId,
-                                        UartReceiveCharCallback_t callback);
-byte_t Uart_UnregisterReceiveCharCallback(COMID_t comId);
+                                        UartReceiveCharCallback_t callback)
+{
+    byte_t ret = 0;
+    if (comId < COM_NUM)
+    {
+        _gCOMList[comId].receive_char_callback = callback;
+        ret = 1;
+    }
+    return ret;
+}
+
+/**
+ * @brief
+ * 取消注册中断中接收字符数据流的函数，这样就能且只能使用Uart_Read读取数据了。
+ *
+ * @param comId 串口号。
+ * @return byte_t 成功1，失败0.
+ */
+byte_t Uart_UnregisterReceiveCharCallback(COMID_t comId)
+{
+    byte_t ret = 0;
+    if (comId < COM_NUM)
+    {
+        _gCOMList[comId].receive_char_callback = NULL;
+        ret = 1;
+    }
+    return ret;
+}
+
+byte_t
+Uart_RegisterReceiveReadyCharCallback(COMID_t comId,
+                                      UartReceiveCharReadyCallback_t callback)
+{
+    byte_t ret = 0;
+    if (comId < COM_NUM)
+    {
+        _gCOMList[comId].receive_char_ready_callback = callback;
+        ret = 1;
+    }
+    return ret;
+}
 
 //
 // InitSciaGpio - This function initializes GPIO pins to function as SCI-A pins
@@ -357,32 +490,47 @@ void InitScicGpio(void)
 }
 
 //
-// errror -
-//
-void error(void)
-{
-    __asm("     ESTOP0"); // Test failed!! Stop!
-    for (;;)
-        ;
-}
-
-//
 // sciaTxFifoIsr -
 //
-__interrupt void
-sciaTxFifoIsr(void)
+__interrupt void sciaTxFifoIsr(void)
 {
-    Uint16 i;
-    for (i = 0; i < 8; i++)
+    /*
+    TXFFIENA:
+    Transmit FIFO interrrupt enable Reset type: SYSRSn
+    0h (R/W) = TX FIFO interrupt is disabled
+    1h (R/W) = TX FIFO interrupt is enabled.
+    This interrupt is triggered whenever the transmit FIFO status (TXFFST) bits
+    match (equal to or less than) the interrupt trigger level bits TXFFIL (bits
+    4-0).
+    */
+
+    if (SciaRegs.SCICTL2.bit.TXRDY == 1)
     {
-        SciaRegs.SCITXBUF = sdataA[i]; // Send data
     }
 
-    for (i = 0; i < 8; i++) // Increment send data for next cycle
+    /*
+    TXEMPTY:
+    Reset type: SYSRSn 0h (R/W) = Transmitter buffer or shift register or both are
+    loaded with data 1h (R/W) = Transmitter buffer and shift registers are both
+    empty
+    */
+    if (SciaRegs.SCICTL2.bit.TXEMPTY == 1)
     {
-        sdataA[i] = (sdataA[i] + 1) & 0x00FF;
+        // Disable transmit FIFO interrrupt
+        SciaRegs.SCIFFTX.bit.TXFFIENA = 0;
+        COM_Dev_t *pDev = &_gCOMList[COM1];
+        if (pDev->write_over_callback != NULL)
+        {
+            pDev->write_over_callback(pDev->write_over_callback_args);
+        }
     }
 
+    /*
+    TXFFINTCLR:
+    Transmit FIFO clear Reset type: SYSRSn
+    0h (R/W) = Write 0 has no effect on TXFIFINT flag bit, Bit reads back a zero
+    1h (R/W) = Write 1 to clear TXFFINT flag in bit 7
+    */
     SciaRegs.SCIFFTX.bit.TXFFINTCLR = 1; // Clear SCI Interrupt flag
     PieCtrlRegs.PIEACK.all |= 0x100;     // Issue PIE ACK
 }
@@ -390,27 +538,61 @@ sciaTxFifoIsr(void)
 //
 // sciaRxFifoIsr -
 //
-__interrupt void
-sciaRxFifoIsr(void)
+__interrupt void sciaRxFifoIsr(void)
 {
-    Uint16 i;
-    for (i = 0; i < 8; i++)
+
+    /*
+    RXRDY: R
+    SCI receiver-ready flag. When a new character is ready to be read from the
+    SCIRXBUF register, the receiver sets this bit, and a receiver interrupt is
+    generated if the RX/BK INT ENA bit (SCICTL2.1) is a 1. RXRDY is cleared by a
+    reading of the SCIRXBUF register, by an active SW RESET, or by a system reset.
+    Reset type: SYSRSn 0h (R/W) = No new character in SCIRXBUF 1h (R/W) =
+    Character ready to be read from SCIRXBUF
+
+    BRKDT: R
+    SCI break-detect flag. The SCI sets this bit when a break condition occurs. A
+    break conditionoccurs when the SCI receiver data line (SCIRXD) remains
+    continuously low for at least ten bits, beginning after a missing first stop
+    bit. The occurrence of a break causes a receiver interrupt to be generated if
+    the RX/BK INT ENA bit is a 1, but it does not cause the receiver buffer to be
+    loaded. A BRKDT interrupt can occur even if the receiver SLEEP bit is set
+    to 1. BRKDT is cleared by an active SW RESET or by a system reset. It is not
+    cleared by receipt of a character after the break is detected. In order to
+    receive more characters, the SCI must be reset by toggling the SW RESET bit or
+    by a system reset. Reset type: SYSRSn 0h (R/W) = No break condition 1h (R/W) =
+    Break condition occurred
+
+    RXWAKE: R
+    Receiver wake-up-detect flag Reset type: SYSRSn 0h (R/W) = No detection of a
+      receiver wake-up condition 1h (R/W) = A value of 1 in this bit indicates
+      detection of a receiver wake-up condition. In the address-bit multiprocessor
+      mode (SCICCR.3 = 1), RXWAKE reflects the value of the address bit for the
+      character contained in SCIRXBUF. In the idle-line multiprocessor mode,
+    RXWAKE is set if the SCIRXD data line is detected as idle. RXWAKE is a
+    read-only flag, cleared by one of the following: - The transfer of the first
+    byte after the address byte to SCIRXBUF (only in non-FIFO mode) - The reading
+    of SCIRXBUF - An active SW RESET - A system reset
+    */
+
+    uint16_t rx_fifo_ele_num = SciaRegs.SCIFFRX.bit.RXFFST;
+    for (uint16_t i = 0; i < rx_fifo_ele_num; i++)
     {
-        rdataA[i] = SciaRegs.SCIRXBUF.all; // Read data
+        rdataB[i] = SciaRegs.SCIRXBUF.all; // Read data
     }
 
-    for (i = 0; i < 8; i++) // Check received data
+    if (SciaRegs.SCIRXST.bit.RXRDY == 1)
     {
-        if (rdataA[i] != ((rdata_pointA + i) & 0x00FF))
-        {
-            error();
-        }
     }
 
-    rdata_pointA = (rdata_pointA + 1) & 0x00FF;
+    if (SciaRegs.SCIRXST.bit.BRKDT == 1)
+    {
+    }
 
-    SciaRegs.SCIFFRX.bit.RXFFOVRCLR = 1; // Clear Overflow flag
-    SciaRegs.SCIFFRX.bit.RXFFINTCLR = 1; // Clear Interrupt flag
+    SciaRegs.SCIFFRX.bit.RXFFOVRCLR =
+        1; // 1h (R/W) = Write 1 to clear RXFFOVF flag in bit 15
+    SciaRegs.SCIFFRX.bit.RXFFINTCLR =
+        1; // 1h (R/W) = Write 1 to clear RXFFINT flag in bit 7
 
     PieCtrlRegs.PIEACK.all |= 0x100; // Issue PIE ack
 }
@@ -418,11 +600,24 @@ sciaRxFifoIsr(void)
 //
 // scibTxFifoIsr -
 //
-__interrupt void
-scibTxFifoIsr(void)
+__interrupt void scibTxFifoIsr(void)
 {
-    // disable the interrupt
-    ScibRegs.SCIFFTX.bit.TXFFIENA = 0;
+    COM_Dev_t *pDev = &_gCOMList[COM2];
+
+    if (ScibRegs.SCICTL2.bit.TXRDY == 1)
+    {
+    }
+
+    if (ScibRegs.SCICTL2.bit.TXEMPTY == 1)
+    {
+        // Disable transmit FIFO interrrupt
+        ScibRegs.SCIFFTX.bit.TXFFIENA = 0;
+
+        if (pDev->write_over_callback != NULL)
+        {
+            pDev->write_over_callback(pDev->write_over_callback_args);
+        }
+    }
 
     ScibRegs.SCIFFTX.bit.TXFFINTCLR = 1; // Clear Interrupt flag
     PieCtrlRegs.PIEACK.all |= 0x100;     // Issue PIE ACK
@@ -431,18 +626,20 @@ scibTxFifoIsr(void)
 //
 // scibRxFifoIsr -
 //
-__interrupt void
-scibRxFifoIsr(void)
+__interrupt void scibRxFifoIsr(void)
 {
     uint16_t rx_fifo_ele_num = ScibRegs.SCIFFRX.bit.RXFFST;
-    for (int i = 0; i < rx_fifo_ele_num; i++)
+    for (uint16_t i = 0; i < rx_fifo_ele_num; i++)
     {
         rdataB[i] = ScibRegs.SCIRXBUF.all; // Read data
     }
 
-    for (int i = 0; i < rx_fifo_ele_num; i++)
+    if (ScibRegs.SCIRXST.bit.RXRDY == 1)
     {
-        ScibRegs.SCITXBUF = rdataB[i]; // Send data
+    }
+
+    if (ScibRegs.SCIRXST.bit.BRKDT == 1)
+    {
     }
 
     ScibRegs.SCIFFRX.bit.RXFFOVRCLR = 1; // Clear Overflow flag
@@ -453,18 +650,23 @@ scibRxFifoIsr(void)
 //
 // scicTxFifoIsr -
 //
-__interrupt void
-scicTxFifoIsr(void)
+__interrupt void scicTxFifoIsr(void)
 {
-    Uint16 i;
-    for (i = 0; i < 8; i++)
+    COM_Dev_t *pDev = &_gCOMList[COM3];
+
+    if (ScicRegs.SCICTL2.bit.TXRDY == 1)
     {
-        ScicRegs.SCITXBUF = sdataC[i]; // Send data
     }
 
-    for (i = 0; i < 8; i++) // Increment send data for next cycle
+    if (ScicRegs.SCICTL2.bit.TXEMPTY == 1)
     {
-        sdataC[i] = (sdataC[i] + 1) & 0x00FF;
+        // Disable transmit FIFO interrrupt
+        ScicRegs.SCIFFTX.bit.TXFFIENA = 0;
+
+        if (pDev->write_over_callback != NULL)
+        {
+            pDev->write_over_callback(pDev->write_over_callback_args);
+        }
     }
 
     ScicRegs.SCIFFTX.bit.TXFFINTCLR = 1; // Clear Interrupt flag
@@ -474,22 +676,21 @@ scicTxFifoIsr(void)
 //
 // scicRxFifoIsr -
 //
-__interrupt void
-scicRxFifoIsr(void)
+__interrupt void scicRxFifoIsr(void)
 {
-    Uint16 i;
-    for (i = 0; i < 8; i++)
+    uint16_t rx_fifo_ele_num = ScicRegs.SCIFFRX.bit.RXFFST;
+    for (uint16_t i = 0; i < rx_fifo_ele_num; i++)
     {
-        rdataC[i] = ScicRegs.SCIRXBUF.all; // Read data
+        rdataB[i] = ScicRegs.SCIRXBUF.all; // Read data
     }
-    for (i = 0; i < 8; i++) // Check received data
+
+    if (ScicRegs.SCIRXST.bit.RXRDY == 1)
     {
-        if (rdataC[i] != ((rdata_pointC + i) & 0x00FF))
-        {
-            error();
-        }
     }
-    rdata_pointC = (rdata_pointC + 1) & 0x00FF;
+
+    if (ScicRegs.SCIRXST.bit.BRKDT == 1)
+    {
+    }
 
     ScicRegs.SCIFFRX.bit.RXFFOVRCLR = 1; // Clear Overflow flag
     ScicRegs.SCIFFRX.bit.RXFFINTCLR = 1; // Clear Interrupt flag
--- HDL/HDL_Uart.h
+++ HDL/HDL_Uart.h
@@ -21,14 +21,14 @@ extern "C"
 #include <stdbool.h>
 #include "ccommon.h"
 
-// Serial Port ID definition
-typedef enum {
-    COM1 = 0, // SCI-A
-    COM2 = 1, // SCI-B
-    COM3 = 2, // SCI-C
-    COM_NUM,
-} COMID_t;
-
+    // Serial Port ID definition
+    typedef enum
+    {
+        COM1 = 0, // SCI-A
+        COM2 = 1, // SCI-B
+        COM3 = 2, // SCI-C
+        COM_NUM,
+    } COMID_t;
 
 #define UART_WORD_LEN_5 5
 #define UART_WORD_LEN_6 6
@@ -43,18 +43,19 @@ typedef enum {
 #define UART_PARITY_ODD 1
 #define UART_PARITY_EVEN 2
 
+    typedef void (*UartWriteOverCallback_t)(void *args);
+    typedef void (*UartReceiveCharCallback_t)(byte_t ch);
+    typedef void (*UartReceiveCharReadyCallback_t)(void);
 
-typedef void (*UartWriteOverCallback_t)(void *args);
-typedef void (*UartReceiveCharCallback_t)(byte_t ch);
-
-void Uart_Init(COMID_t comId, uint32_t baud, uint32_t wordLen, uint32_t stopBit, uint32_t parity);
-uint32_t Uart_Write(COMID_t comId, const byte_t *writeBuf, uint32_t uLen);
-uint32_t Uart_Read(COMID_t comId, byte_t *pBuf, uint32_t uiLen);
-uint32_t Uart_AvailableBytes(COMID_t comId);
-uint32_t Uart_EmptyReadBuffer(COMID_t comId);
-byte_t Uart_SetWriteOverCallback(COMID_t comId, UartWriteOverCallback_t callback, void *args);
-byte_t Uart_RegisterReceiveCharCallback(COMID_t comId, UartReceiveCharCallback_t callback);
-byte_t Uart_UnregisterReceiveCharCallback(COMID_t comId);
+    void Uart_Init(COMID_t comId, uint32_t baud, uint32_t wordLen, uint32_t stopBit, uint32_t parity);
+    uint32_t Uart_Write(COMID_t comId, const byte_t *writeBuf, uint32_t uLen);
+    uint32_t Uart_Read(COMID_t comId, byte_t *pBuf, uint32_t uiLen);
+    uint32_t Uart_AvailableBytes(COMID_t comId);
+    uint32_t Uart_EmptyReadBuffer(COMID_t comId);
+    byte_t Uart_SetWriteOverCallback(COMID_t comId, UartWriteOverCallback_t callback, void *args);
+    byte_t Uart_RegisterReceiveCharCallback(COMID_t comId, UartReceiveCharCallback_t callback);
+    byte_t Uart_RegisterReceiveReadyCharCallback(COMID_t comId, UartReceiveCharReadyCallback_t callback);
+    byte_t Uart_UnregisterReceiveCharCallback(COMID_t comId);
 
 #ifdef __cplusplus
 }
