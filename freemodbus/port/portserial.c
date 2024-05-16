cafae470003862f9013b394a843282f7372108ae
MisakaMikoto128 <liuyuanlins@outlook.com>
2024-05-16 12:42:28 +0800

    format

--- freemodbus/port/portserial.c
+++ freemodbus/port/portserial.c
@@ -27,9 +27,9 @@
 #include "BFL_RS485.h"
 #include "ccommon.h"
 #include <stddef.h>
-#define RTU_COM COM2
-#define RTU_COM_RS485 RS485_NONE
-#define SciregUsed ScibRegs
+#define RTU_COM COM3
+#define RTU_COM_RS485 RS485_2
+#define SciregUsed ScicRegs
 /* ----------------------- Modbus includes ----------------------------------*/
 #include "mb.h"
 #include "mbport.h"
@@ -37,36 +37,44 @@
 /* ----------------------- Defines ------------------------------------------*/
 void serialReceiveOneByteISR(void);
 
-void serialSentOneByteISR(void*);
-
+void serialSentOneByteISR(void *);
 
 /* ----------------------- Static variables ---------------------------------*/
 
 /* ----------------------- Start implementation -----------------------------*/
 void vMBPortSerialEnable(BOOL xRxEnable, BOOL xTxEnable)
 {
-    if (xTxEnable) {
+    if (xTxEnable)
+    {
         BFL_RS485_Take_Bus(RTU_COM_RS485);
-    } else {
+    }
+    else
+    {
         BFL_RS485_Release_Bus(RTU_COM_RS485);
     }
 
-    if (xTxEnable) {
+    if (xTxEnable)
+    {
         // UART_EnableIT_TC(USART3); // 使能发送完成中断
-       //  TX FIFO Interrupt Enable
+        //  TX FIFO Interrupt Enable
         SciregUsed.SCIFFTX.bit.TXFFIENA = 1;
-    } else {
+    }
+    else
+    {
         // UART_DisableIT_TC(USART3); // 禁能发送完成中断
-                // Disable transmit FIFO interrrupt
+        // Disable transmit FIFO interrrupt
         SciregUsed.SCIFFTX.bit.TXFFIENA = 0;
     }
-    
-    if (xRxEnable) {
+
+    if (xRxEnable)
+    {
         // UART_EnableIT_RXNE(USART3);
-            SciregUsed.SCIFFRX.bit.RXFFIENA = 1; // 接收FIFO中断使能
-    } else {
+        SciregUsed.SCIFFRX.bit.RXFFIENA = 1; // 接收FIFO中断使能
+    }
+    else
+    {
         // UART_DisableIT_RXNE(USART3);
-            SciregUsed.SCIFFRX.bit.RXFFIENA = 0; 
+        SciregUsed.SCIFFRX.bit.RXFFIENA = 0;
     }
 }
 
@@ -81,25 +89,26 @@ BOOL xMBPortSerialInit(UCHAR ucPort, ULONG ulBaudRate, UCHAR ucDataBits, eMBPari
     UNUSED(ucPort);
     UNUSED(ucDataBits);
     uint32_t stopBit = UART_STOP_BIT_1;
-    uint32_t parity  = UART_PARITY_NONE;
+    uint32_t parity = UART_PARITY_NONE;
 
     stopBit = eParity == MB_PAR_NONE ? UART_STOP_BIT_2 : UART_STOP_BIT_1;
-    switch (eParity) {
-        case MB_PAR_NONE:
-            parity = UART_PARITY_NONE;
-            break;
-        case MB_PAR_ODD:
-            parity = UART_PARITY_ODD;
-            break;
-        case MB_PAR_EVEN:
-            parity = UART_PARITY_EVEN;
-            break;
-        default:
-            break;
+    switch (eParity)
+    {
+    case MB_PAR_NONE:
+        parity = UART_PARITY_NONE;
+        break;
+    case MB_PAR_ODD:
+        parity = UART_PARITY_ODD;
+        break;
+    case MB_PAR_EVEN:
+        parity = UART_PARITY_EVEN;
+        break;
+    default:
+        break;
     }
 
-    // BFL_RS485_Init(RTU_COM_RS485, ulBaudRate, UART_WORD_LEN_8, stopBit, parity);
-    Uart_Init(RTU_COM, ulBaudRate, UART_WORD_LEN_8, stopBit, parity);
+    BFL_RS485_Init(RTU_COM_RS485, ulBaudRate, UART_WORD_LEN_8, stopBit, parity);
+    // Uart_Init(RTU_COM, ulBaudRate, UART_WORD_LEN_8, stopBit, parity);
 
     // 因为我使用的这个库已经自己实现了接收完成中断，所以这里没有单独列出中断函数来,按照文章说明的意思来即可
     Uart_RegisterReceiveReadyCharCallback(RTU_COM, serialReceiveOneByteISR);
@@ -126,7 +135,7 @@ void serialReceiveOneByteISR(void)
     pxMBFrameCBByteReceived();
 }
 
-void serialSentOneByteISR(void*arg)
+void serialSentOneByteISR(void *arg)
 {
     UNUSED(arg);
     pxMBFrameCBTransmitterEmpty();
@@ -140,6 +149,6 @@ void EnterCriticalSection(void)
 
 void ExitCriticalSection(void)
 {
-  // 恢复中断
-  _enable_interrupts();
+    // 恢复中断
+    _enable_interrupts();
 }
