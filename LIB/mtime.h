0b57631fd69d41a0068cee85a7311ad3ac9a4334
liuyuanlin <5270497+liuyuanlin6445@user.noreply.gitee.com>
2024-11-11 00:09:43 +0800

    完善mtime_t的注释

--- LIB/mtime.h
+++ LIB/mtime.h
@@ -13,43 +13,42 @@
 #define MTIME_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 #include <stdint.h>
 #include <stdbool.h>
 #include "ccommon.h"
+    /* 自定义的时间结构体 */
+    typedef struct
+    {
+        uint16_t nYear; /**< 年份（例如 2024） */
+        byte_t nMonth;  /**< 月份（1-12） */
+        byte_t nDay;    /**< 日期（1-31） */
+        byte_t nHour;   /**< 小时（0-23） */
+        byte_t nMin;    /**< 分钟（0-59） */
+        byte_t nSec;    /**< 秒钟（0-59） */
+        byte_t nWeek;   /* 0 = Sunday */
+        uint16_t wSub;  /**< 亚秒（未使用，视具体需求而定） */
+    } mtime_t;
 
-/* 自定义的时间结构体 */
-typedef struct
-{
-    uint16_t nYear;
-    byte_t nMonth;
-    byte_t nDay;
-    byte_t nHour;
-    byte_t nMin;
-    byte_t nSec;
-    byte_t nWeek; /* 0 = Sunday */
-    uint16_t wSub; // 亚秒
-} mtime_t;
+    /**
+     * @brief 根据给定的日期得到对应的星期
+     *
+     * @param year
+     * @param month
+     * @param day
+     * @return byte_t 0 = Sunday
+     */
+    byte_t mtime_get_week(uint16_t year, byte_t month, byte_t day);
+    void mtime_unix_sec_2_time(uint32_t utc_sec, mtime_t *result); // 根据UTC时间戳得到对应的日期
+    uint32_t mtime_2_unix_sec(mtime_t *currTime);                  // 根据时间计算出UTC时间戳
+    char *mtime_format(uint32_t utc_sec, char *pBuf);              // 根据UTC时间戳得到对应的日期字符串
 
-/**
- * @brief 根据给定的日期得到对应的星期
- *
- * @param year
- * @param month
- * @param day
- * @return byte_t 0 = Sunday
- */
-byte_t mtime_get_week(uint16_t year, byte_t month, byte_t day);
-void mtime_unix_sec_2_time(uint32_t utc_sec, mtime_t *result); // 根据UTC时间戳得到对应的日期
-uint32_t mtime_2_unix_sec(mtime_t *currTime);                  // 根据时间计算出UTC时间戳
-char *mtime_format(uint32_t utc_sec, char *pBuf);              // 根据UTC时间戳得到对应的日期字符串
-
-void mtime_add_hours(mtime_t *currTime, uint32_t hours);
-void mtime_sub_hours(mtime_t *currTime, uint32_t hours);
-byte_t mtime_is_equal(mtime_t *currTime, mtime_t *targetTime);
-
+    void mtime_add_hours(mtime_t *currTime, uint32_t hours);
+    void mtime_sub_hours(mtime_t *currTime, uint32_t hours);
+    byte_t mtime_is_equal(mtime_t *currTime, mtime_t *targetTime);
 
 #define MINUTE(x) ((x) * 1ull)
 #define SEC(x) ((x) * 1ull)
