1423811f5f28c8b3c2c3b0d03eb5c5e1d72cb6aa
liuyuanlin <5270497+liuyuanlin6445@user.noreply.gitee.com>
2024-11-11 12:39:56 +0800

    30秒新校准一次时间

--- HDL/HDL_RTC.c
+++ HDL/HDL_RTC.c
@@ -33,7 +33,7 @@ struct softRTC_t
 
 static struct softRTC_t softRTC = {0};
 static struct softRTC_t *pSoftRTC = &softRTC;
-static uint32_t reSyncPeriod = SECOND_TO_MS(10); // 3秒新校准一次
+static uint32_t reSyncPeriod = SECOND_TO_MS(30); // 30秒新校准一次
 static uint32_t elapsedTime = 0;
 static void HDL_RTC_Update()
 {
