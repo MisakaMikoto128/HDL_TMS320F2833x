c0a37d1e0ea15aad280a9092c9e8b76ac5f6829e
liuyuanlin <5270497+liuyuanlin6445@user.noreply.gitee.com>
2024-12-11 23:02:15 +0800

    温度传感器添加错误帧过滤，避免硬件错误但是CRC却没有判断出来的问题

--- APP/B1_CapacitanceTemperatureMeasure.c
+++ APP/B1_CapacitanceTemperatureMeasure.c
@@ -25,13 +25,15 @@ static const byte_t request_cmd[] = {0x01, 0x03, 0x00, 0x00, 0x00, 0x09, 0x85, 0
 #define TEMP_RS485 RS485_2
 void B1_CapacitanceTemperatureMeasure_Init()
 {
-    BFL_RS485_Init(TEMP_RS485, 38400, UART_WORD_LEN_8, UART_STOP_BIT_1,UART_PARITY_NONE); // 隔离 MAX3485
+    BFL_RS485_Init(TEMP_RS485, 38400, UART_WORD_LEN_8, UART_STOP_BIT_1, UART_PARITY_NONE); // 隔离 MAX3485
 
     for (size_t i = 0; i < (sizeof(g_pSysInfo->capTemp) / sizeof(g_pSysInfo->capTemp[0])); i++)
     {
         g_pSysInfo->capTemp[i] = 0;
     }
 }
+
+#define MODBUS_MIN_MSG_LENGTH 4
 
 void B1_CapacitanceTemperatureMeasure_Poll()
 {
@@ -53,44 +55,51 @@ void B1_CapacitanceTemperatureMeasure_Poll()
         g_AppMainInfo.capTempSensorRequestStage = 0;
 
         uint32_t readLen = BFL_RS485_Read(TEMP_RS485, g_AppMainInfo.buffer, sizeof(g_AppMainInfo.buffer));
-        if (readLen > 0)
+        bool parseSuccess = false;
+
+        if (readLen >= MODBUS_MIN_MSG_LENGTH)
         {
-            g_pSysInfo->capTempSensorTransmitConnect = true;
-            g_AppMainInfo.capTempSensorLastRevMsgTime_Ms = HDL_CPU_Time_GetTick();
             byte_t *dataBuffer = g_AppMainInfo.buffer;
             // Check CRC
             if (CRC16_Modbus(dataBuffer, readLen) == 0)
             {
-                // Parse data eg: 01 03 06 FF FF 01 05 00 00 30 93
-                int regNum = dataBuffer[2] / 2;
-                byte_t *regStart = &dataBuffer[3];
-                uint16_t reg = 0;
-                int tempChannelNum = sizeof(g_pSysInfo->capTemp) / sizeof(g_pSysInfo->capTemp[0]);
-                regNum = regNum > tempChannelNum ? tempChannelNum : regNum;
-                for (int i = 0; i < regNum; i++)
+                // Check function code and slaver address
+                if (dataBuffer[0] == request_cmd[0] && dataBuffer[1] == request_cmd[1])
                 {
-                    reg = regStart[i * 2] << 8 | regStart[i * 2 + 1];
-                    if (reg == 0xFFFF)
+                    // Mark successful parsing
+                    parseSuccess = true;
+                    g_pSysInfo->capTempSensorTransmitConnect = true;
+                    g_AppMainInfo.capTempSensorLastRevMsgTime_Ms = HDL_CPU_Time_GetTick();
+
+                    // Parse data eg: 01 03 06 FF FF 01 05 00 00 30 93
+                    int regNum = dataBuffer[2] / 2;
+                    byte_t *regStart = &dataBuffer[3];
+                    uint16_t reg = 0;
+                    int tempChannelNum = sizeof(g_pSysInfo->capTemp) / sizeof(g_pSysInfo->capTemp[0]);
+                    regNum = regNum > tempChannelNum ? tempChannelNum : regNum;
+                    for (int i = 0; i < regNum; i++)
                     {
-                        g_pSysInfo->capTemp[i] = 0;
-                        g_pSysInfo->capTempSensorFault[i] = true;
-                    }
-                    else
-                    {
-                        g_pSysInfo->capTemp[i] = reg * 0.1f;
-                        g_pSysInfo->capTempSensorFault[i] = false;
+                        reg = regStart[i * 2] << 8 | regStart[i * 2 + 1];
+                        if (reg == 0xFFFF)
+                        {
+                            g_pSysInfo->capTemp[i] = 0;
+                            g_pSysInfo->capTempSensorFault[i] = true;
+                        }
+                        else
+                        {
+                            g_pSysInfo->capTemp[i] = reg * 0.1f;
+                            g_pSysInfo->capTempSensorFault[i] = false;
+                        }
                     }
                 }
             }
         }
-        else
+
+        if (g_pSysInfo->capTempSensorTransmitConnect == true && !parseSuccess)
         {
-            if (g_pSysInfo->capTempSensorTransmitConnect == true)
+            if (HDL_CPU_Time_GetTick() - g_AppMainInfo.capTempSensorLastRevMsgTime_Ms > 2000)
             {
-                if (HDL_CPU_Time_GetTick() - g_AppMainInfo.capTempSensorLastRevMsgTime_Ms > 2000)
-                {
-                    g_pSysInfo->capTempSensorTransmitConnect = false;
-                }
+                g_pSysInfo->capTempSensorTransmitConnect = false;
             }
         }
     }
